93b2b7f61793347599a01608799b41294d785bcf
martinhacker <linsi.yao@163.com>
2017-10-16 21:12:43 +0800

    审核信息

--- resources/views/partial/banner.blade.php
+++ resources/views/partial/banner.blade.php
@@ -8,12 +8,13 @@
         <div class="row float-area">
             <div class="col-xs-10 col-xs-offset-2 col-md-4 col-md-offset-2 banner-text">
                 <h2>北师人 Part-time 社区</h2>
-                <h4>更安全方便的找到心仪的 Job</h4>
+                {{--<h4>更安全方便的找到心仪的 Job</h4>--}}
+                <h4>更安全方便的找到你走丢的卡</h4>
                 <p>我们坚信，你有无数个加入我们的理由！</p>
             </div>
-            <div class="hidden-xs col-md-offset-1 col-md-3 banner-form">
-                <simple-register></simple-register>
-            </div>
+            {{--<div class="hidden-xs col-md-offset-1 col-md-3 banner-form">--}}
+                {{--<simple-register></simple-register>--}}
+            {{--</div>--}}
         </div>
     </div>
 </header>
