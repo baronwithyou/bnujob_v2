a5a2ccb4b531d5bd77a5e61f5bca50f0e6ba8f4a
martinhacker <linsi.yao@163.com>
2017-10-16 21:19:43 +0800

    修改页脚

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <div class="container-fluid">
         <div class="row">
-            <p class="text-center">粤ICP备17044003号</p>
+            <p class="text-center"><a href="http://www.miitbeian.gov.cn">粤ICP备17044003号</a></p>
         </div>
     </div>
 </div>
